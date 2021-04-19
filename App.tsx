cf22fa188ffc36d5b02be4757c75944ec946d6fa
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-04-19 10:43:07 -0300

    add splash screen

--- App.tsx
+++ App.tsx
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View,Text,StyleSheet} from 'react-native'
 
-export default function App() {
-  return (
+export default function App(){
+  return(
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>aaa</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flex:1,
     justifyContent: 'center',
-  },
-});
+    alignItems: 'center',
+  }
+})
+
