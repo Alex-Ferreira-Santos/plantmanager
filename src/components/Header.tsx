cb90f81a661bed154d1dc79d689fa71a825fcdb3
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-04-21 11:49:10 -0300

    creating header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import {View,Text,StyleSheet, Image} from 'react-native'
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+import {getStatusBarHeight} from 'react-native-iphone-x-helper'
+import UserImg from '../assets/Alex.png'
+
+export function Header(){
+    return(
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.greetings}>Olá,</Text>
+                <Text style={styles.userName}>Rodrigo</Text>
+            </View>
+            <Image source={UserImg} style={styles.image}/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent:'space-between',
+        alignItems: 'center',
+        paddingVertical: 20,
+        marginTop: getStatusBarHeight(),
+        padding: 20,
+    },
+    greetings:{
+        fontSize: 32,
+        color: colors.heading,
+        fontFamily: fonts.text
+    },
+    userName:{
+        fontSize: 32,
+        fontFamily: fonts.heading,
+        color: colors.heading,
+        lineHeight: 40,
+    },
+    image:{
+        width: 70,
+        height: 70,
+        borderRadius: 40
+    }
+})
--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -3,20 +3,25 @@ import { View,Text,SafeAreaView,StyleSheet } from 'react-native';
 import {Button} from '../components/Button'
 import colors from '../styles/colors'
 import fonts from '../styles/fonts'
+import { useNavigation } from '@react-navigation/core';
 
 export function Confirmation(){
-  return (
-    <SafeAreaView style={styles.container}>
-        <View style={styles.content}>
-            <Text style={styles.emoji}>😁</Text>
-            <Text style={styles.title}>Prontinho</Text>
-            <Text style={styles.subtitle}>Agora vamos começar a cuidar das suas plantinhas com muito cuidado</Text>
-            <View style={styles.footer}>
-                <Button title="começar"/>
+    const navigation = useNavigation()
+    function handleMoveOn(){
+        navigation.navigate('PlantSelect')
+    }
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.content}>
+                <Text style={styles.emoji}>😁</Text>
+                <Text style={styles.title}>Prontinho</Text>
+                <Text style={styles.subtitle}>Agora vamos começar a cuidar das suas plantinhas com muito cuidado</Text>
+                <View style={styles.footer}>
+                    <Button title="começar" onPress={handleMoveOn}/>
+                </View>
             </View>
-        </View>
-       
-    </SafeAreaView>
+        
+        </SafeAreaView>
   );
 }
 
